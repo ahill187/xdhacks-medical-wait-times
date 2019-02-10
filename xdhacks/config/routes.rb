e17c08cd33b92b2170e34ee55fd94699185d025f
ahill187 <ainsleighhill@gmail.com>
2019-02-09 19:27:09 -0800

    Added StaticPages controller for home page

--- xdhacks/config/routes.rb
+++ xdhacks/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
+  match '/home',    to: 'static_pages#home',    via: 'get'
+  #get 'layouts/application'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
